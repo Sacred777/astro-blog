7a15ac61e6e76bbb8f1818058b25c07d226418fa
Oleg Korneev <olvikorneev@gmail.com>
2026-03-01 19:06:24 +0300

    ci: add deploy configs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  base: '/astro-blog/', // замените на имя вашего репозитория
+  output: 'static',
+});
