4c29047e7a7c25ca8deae6810bf6dc719dd4f217
Oleg Korneev <olvikorneev@gmail.com>
2026-03-02 15:11:32 +0300

    build: change base path for dev

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  base: '/astro-blog/', // замените на имя вашего репозитория
+  base: process.env.BASE_PATH || '/astro-blog/',
   output: 'static',
 });
